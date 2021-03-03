07217cb157fb6192d8978ba51fb0240ed96aa8d8
Lazar Davidovic <laki@hotmail.rs>
2021-03-03 23:24:27 +0100

    added function for loading fast5 files.
    *Note: np is a python module, not entirely sure if I should look for a
    replacement in julia or manually convert the array to Float32 type?

--- src/NanoporeMultiEventalign.jl
+++ src/NanoporeMultiEventalign.jl
@@ -1,6 +1,7 @@
 __precompile__(true)
 
 module NanoporeMultiEventalign
+using HDF5
 
 include("utils.jl")
 include("persistence.jl")
--- src/persistence.jl
+++ src/persistence.jl
@@ -1,1 +1,23 @@
 # Here put file load/file write functions
+function loadfast5(path)
+
+    hfile = h5open(path, "r")
+    channel_info = hfile["UniqueGlobalKey/channel_id"]
+    raw_signal = hfile["Raw/Reads/Read_242"]
+
+    # Store all the values used for normalization
+    range = read(attributes(channel_info)["range"])
+    digitisation = read(attributes(channel_info)["digitisation"])
+    offset = Int(read(attributes(channel_info)["offset"]))
+    raw = read(raw_signal["Signal"]) #Int16 array
+
+    # Normalization
+    normalized_data = []
+    scaling = range / digitisation
+    for i in raw
+        x = scaling * (i + offset)
+        push!(normalized_data, x)
+    end
+    return normalized_data
+
+end
