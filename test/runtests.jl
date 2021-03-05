cb9a46817b98be375511d5d3d1275d512d995ed4
JokingHero <kornellabun@gmail.com>
2021-03-05 09:25:12 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,8 @@ end
 @testset "persistence.jl" begin
     @testset "loadnanoporefast5" begin
         @test loadnanoporefast5(
-            "test/sample_data/fast5/control/0/" * 
-            "0a4c39b4-14c0-4d49-bce9-81486627062c.fast5")[6] == 76.81596
+            "sample_data/fast5/control/0/" *
+            "0a4c39b4-14c0-4d49-bce9-81486627062c.fast5")[6] == Float32(76.81596)
     end
     @testset "loadfasta" begin
         @test convert(String, loadfasta(
