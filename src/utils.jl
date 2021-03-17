e24e852fa2e5300b376399e8846e4d513b79d258
Thohil <51682076+Thohil@users.noreply.github.com>
2021-03-17 15:39:16 +0100

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -4,7 +4,7 @@ Gets the corosponding mean and stdv from a fasta sequense using kmer data.
 Takes in fasta data loaded from loadfasta and kmers loaded from loadkmers.
 Returns a list of tuples where the values are {mean, stdv}.
 """
-function fasta_to_kmer_valus(fastadata, kmers)
+function fasta_to_kmer_values(fastadata, kmers)
     datastring = convert(String, fastadata[1])
     fastameans = []
     # gets the length of a single kmer key
