162c2fcd9ebc318471084a671ef4e9d0e3f047d5
Tho.hil <tho.hillesland@gmail.com>
2021-04-18 20:24:51 +0200

    test

--- src/NanoporeMultiEventalign.jl
+++ src/NanoporeMultiEventalign.jl
@@ -12,10 +12,12 @@ include("persistence.jl")
 include("dtw.jl")
 include("ChangePointDetection.jl")
 
-X = [1.3,3.3,4.3,5.3]
-C = [3.3,04.3,5.3,6.2]
-
-println(lsdd(X,C))
+ts1 = rand(50)
+ts2 = 1.5 * rand(50) .+ 1
+ts = vcat(ts1, ts2)
+profile = lsdd_profile(ts; window = 1)
+points = getpoints(profile, threshold = 1.3)
+print(points)
 
 export nanopore_dtw
 export loadnanoporefast5, loadfasta, loadkmers, fasta_to_kmer_values
