a677e4a2cedb625bc1d224fd48708f44d1f05330
Tho.hil <tho.hillesland@gmail.com>
2021-04-18 19:26:57 +0200

    test

--- src/NanoporeMultiEventalign.jl
+++ src/NanoporeMultiEventalign.jl
@@ -6,14 +6,16 @@ using FASTX
 using HDF5
 using Changepoints
 using DynamicAxisWarping
+using ChangePointDetection
 
 include("utils.jl")
 include("persistence.jl")
 include("dtw.jl")
 
+X = [1.3,3.3,4.3,5.3]
+C = [3.3,04.3,5.3,6.2]
 
-
-
+println(lsdd(X,C))
 
 export nanopore_dtw
 export loadnanoporefast5, loadfasta, loadkmers, fasta_to_kmer_values
